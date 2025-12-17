79981656935ad1f7ffb1357b8adbcae9c30dd69e
walungaira-design <walungaira@gmail.com>
2025-12-17 18:41:04 +0545

    Update autosh.php

--- autosh.php
+++ autosh.php
@@ -1,8 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-ob_start();
+error_reporting(E_ALL & ~E_DEPRECATED);
 
 $maxRetries = 5;
 $retryCount = 0;
